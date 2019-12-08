b18de76abe77c4e9d2f997088149ad83d189e8a1
ricardodf <39528804+ricardodf@users.noreply.github.com>
2019-12-07 21:46:09 -0600

    Ruta con Token

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require ('express');
 const randomize = require('randomatic');
 const path = require('path');
-const port = process.env.PORT || 5000;  // Talvez no sea en en el 5000, asi que primero checo la variable de ambiente.
+const port = process.env.port || 5000;  // Talvez no sea en en el 5000, asi que primero checo la variable de ambiente.
 
 const app = express();
 
@@ -11,6 +11,7 @@ app.use(express.static(path.join(__dirname, 'front')));
 
 app.use(express.json());
 app.use('/api/users', require('./routes/users'));
+app.use('/api/token', require('./routes/token'));
 
 
 app.listen(port, () => {
